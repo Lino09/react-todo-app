90848ba1792cdafe0530bce16cf4150d2c2e5455
Lino09 <onosmx@gmail.com>
2021-11-29 15:57:11 -0600

    Set Dev Branch

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./components/TodoContainer"
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+ReactDOM.render(<TodoContainer />, document.getElementById("root" ))
