a90e801622614bf776be7b23a278a79488ebd415
Lino09 <onosmx@gmail.com>
2021-11-30 08:32:42 -0600

    Add class names

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,7 +7,10 @@ class TodoItem extends React.Component {
       type="checkbox" 
       checked={this.props.todo.completed} 
       onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
-      <label>{this.props.todo.title}</label>
+      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        Delete
+      </button>
+      {this.props.todo.title}
     </li>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./components/TodoContainer"
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root" ))
+import "./App.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
